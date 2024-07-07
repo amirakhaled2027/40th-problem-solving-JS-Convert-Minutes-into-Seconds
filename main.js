3f63cb2d570dce6772e7ede8fe5e0af3f3c3644a
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-07 19:13:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Write a function that takes an integer minutes and converts it to seconds.
+
+function conversion(min) {
+    return min * 60;
+};
+console.log(conversion(5));
+console.log(conversion(3));
+console.log(conversion(456));
